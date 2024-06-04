848b18e26cde587bab2a930e2a87a98427729a42
andreiashu <andreiashu@gmail.com>
2024-06-04 13:39:28 +0700

    🍯 Clear readme; Generated Asccinema; Updated code to point to Mainnet addresses;

--- script/BugReplication.s.sol
+++ script/BugReplication.s.sol
@@ -1,27 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.6.12; // @todo check version
+pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
-import {Script, console} from "forge-std/Script.sol";
+import "forge-std/Test.sol";
+import {Script, console2} from "forge-std/Script.sol";
 import {TokenArrays} from "./../src/util/TokenArrays.sol";
 import {IComposableStablePoolFactory} from "./../src/interfaces/IComposableStablePoolFactory.sol";
 
-contract BugReplication is Script {
-    address constant adminAddress = 0x235A2ac113014F9dcb8aBA6577F20290832dDEFd;
-    address constant VAULT = 0x31d6656262CbC8262F643b6e3b2644c528940D79;
+contract BugReplication is Script, Test {
+    // mainnet Balancer V2 Vault
+    address constant VAULT = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
+    // mainnet Balancer V2 CmposableStablePoolFactory
     address constant cstFactoryAddress =
-        0x398D4CFB5D29d18BaA149497656904F2e8814EFb;
+        0x5B42eC6D40f7B7965BE5308c70e2603c0281C1E9;
+
+    // first wallet from mnemonic "test test test test test test test test test test test junk"
+    address constant ANVIL_PUB_KEY = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    uint256 constant ANVIL_PRIV_KEY =
+        0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
     function run() public {
-        vm.startBroadcast();
-        console.log("hello");
         _deployCSP(
             cstFactoryAddress,
             "USDT/USDC CSP",
-            0x499F0a05582685f8728fF2f61F5F20D8001b7D73,
-            0x39A632989Ae533a2bc1b66f17f5642906AB3F6c5
+            0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, // USDC
+            0xdAC17F958D2ee523a2206206994597C13D831ec7 // USDT
         );
-        vm.stopBroadcast();
     }
 
     function _deployCSP(
@@ -40,6 +44,17 @@ contract BugReplication is Script {
         tokenRateCacheDurations[0] = 10800;
         tokenRateCacheDurations[1] = 10800;
 
+        vm.startBroadcast(
+            // anvil's wallet #0 private key
+            ANVIL_PRIV_KEY
+        );
+
+        bytes32 someBytes32 = keccak256(
+            abi.encode(address(this), vm.getNonce(ANVIL_PUB_KEY))
+        );
+        console2.log("someBytes32: ");
+        console2.logBytes32(someBytes32);
+
         address csp = IComposableStablePoolFactory(cstFactory).create(
             _name,
             _name,
@@ -49,12 +64,12 @@ contract BugReplication is Script {
             TokenArrays.createValueList(10800, 10800),
             false,
             500000000000000,
-            adminAddress,
-            // @TODO update
-            0x5959d140743dcfe9f7e9a6b2ede1b84d8a1320f08c43029b4fcf26ecc33041d0 // @todo check salt
+            makeAddr("asdf"),
+            someBytes32
         );
+        vm.stopBroadcast();
 
-        console.log("pool address: ", csp);
+        console2.log("pool address: ", csp);
 
         return csp;
     }
